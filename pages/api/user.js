43a506bd1bc9d61dfe4885d484382f0c48020f93
jonathanprozzi <jonathanprozzi@gmail.com>
2021-03-19 15:25:45 -0400

    resolved issue with user api route

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,19 +1,23 @@
 import Iron from "@hapi/iron";
 import CookieService from "../../lib/cookie";
 
-export default async (req, res) => {
+const userApi = async (req, res) => {
   let user;
-
   try {
     user = await Iron.unseal(
-      CookieService.getAuthToken(
-        req.cookies,
-        process.env.ENCRYPTION_SECRET,
-        Iron.defaults
-      )
+      CookieService.getAuthToken(req.cookies),
+      process.env.ENCRYPTION_SECRET,
+      Iron.defaults
     );
-  } catch {
+  } catch (error) {
     res.status(401).end();
   }
+
+  // now we have access to the data inside of user
+  // and we could make database calls or just send back what we have
+  // in the token.
+
   res.json(user);
 };
+
+export default userApi;
